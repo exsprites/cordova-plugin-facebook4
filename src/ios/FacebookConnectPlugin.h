9dbdd51670b2caff3b43ef93d11242843982b62b
Nam Pham <exsprites@yahoo.com>
2020-11-24 14:06:46 +0700

    Removed: LoginKit and ShareKit

--- src/ios/FacebookConnectPlugin.h
+++ src/ios/FacebookConnectPlugin.h
@@ -11,25 +11,26 @@
 
 #import <Foundation/Foundation.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
-#import <FBSDKLoginKit/FBSDKLoginKit.h>
-#import <FBSDKShareKit/FBSDKShareKit.h>
+// #import <FBSDKLoginKit/FBSDKLoginKit.h>
+// #import <FBSDKShareKit/FBSDKShareKit.h>
 #import <Cordova/CDV.h>
 #import "AppDelegate.h"
 
-@interface FacebookConnectPlugin : CDVPlugin <FBSDKSharingDelegate, FBSDKGameRequestDialogDelegate>
+// @interface FacebookConnectPlugin : CDVPlugin <FBSDKSharingDelegate, FBSDKGameRequestDialogDelegate>
+@interface FacebookConnectPlugin : CDVPlugin
 - (void)setAutoLogAppEventsEnabled:(CDVInvokedUrlCommand *)command;
 - (void)setAdvertiserIDCollectionEnabled:(CDVInvokedUrlCommand *)command;
 - (void)isAutoLogAppEventsEnabled:(CDVInvokedUrlCommand *)command;
 - (void)isAdvertiserIDCollectionEnabled:(CDVInvokedUrlCommand *)command;
-- (void)getLoginStatus:(CDVInvokedUrlCommand *)command;
-- (void)getAccessToken:(CDVInvokedUrlCommand *)command;
+// - (void)getLoginStatus:(CDVInvokedUrlCommand *)command;
+// - (void)getAccessToken:(CDVInvokedUrlCommand *)command;
 - (void)logEvent:(CDVInvokedUrlCommand *)command;
 - (void)logPurchase:(CDVInvokedUrlCommand *)command;
-- (void)login:(CDVInvokedUrlCommand *)command;
-- (void)checkHasCorrectPermissions:(CDVInvokedUrlCommand *)command;
-- (void)logout:(CDVInvokedUrlCommand *)command;
-- (void)graphApi:(CDVInvokedUrlCommand *)command;
-- (void)showDialog:(CDVInvokedUrlCommand *)command;
-- (void)getDeferredApplink:(CDVInvokedUrlCommand *) command;
+// - (void)login:(CDVInvokedUrlCommand *)command;
+// - (void)checkHasCorrectPermissions:(CDVInvokedUrlCommand *)command;
+// - (void)logout:(CDVInvokedUrlCommand *)command;
+// - (void)graphApi:(CDVInvokedUrlCommand *)command;
+// - (void)showDialog:(CDVInvokedUrlCommand *)command;
+// - (void)getDeferredApplink:(CDVInvokedUrlCommand *) command;
 - (void)activateApp:(CDVInvokedUrlCommand *)command;
 @end
